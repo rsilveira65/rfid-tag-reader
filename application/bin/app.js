88b7dbdde308bbc5de31a6739faedcfb55db6193
Rafael Silveira <rafael.silveira@possible.com>
2018-09-15 19:09:12 -0300

    updating files

--- application/bin/app.js
+++ application/bin/app.js
@@ -17,7 +17,7 @@ const run = () => {
     const uid = response.data;
 
     console.log("Card read UID: %s %s %s %s", uid[0].toString(16), uid[1].toString(16), uid[2].toString(16), uid[3].toString(16));
-    ledService.on();
+    ledService.blink();
 
     rfidService.stop();
 };
--- application/services/ledService.js
+++ application/services/ledService.js
@@ -21,7 +21,23 @@ const off = (err) => {
         })
 }
 
+const blink = (err) => {
+    gpiop.setup(7, gpio.DIR_OUT)
+        .then(() => {
+            return gpiop.write(7, true)
+        })
+        .then(() => {
+            setInterval(() => {
+                return gpiop.write(7, off)
+            }, 500);
+        })
+        .catch((err) => {
+            console.log('Error: ', err.toString())
+        })
+}
+
 module.exports = {
     on,
-    off
+    off,
+    blink
 };
