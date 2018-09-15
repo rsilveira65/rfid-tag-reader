43690476f274125ca8fdbffa7f9681634a627e4f
Rafael Silveira <rafael.silveira@possible.com>
2018-09-15 18:07:09 -0300

    Updating project structure.

---
+++ application/bin/app.js
@@ -0,0 +1,35 @@
+/**
+ * Local Modules
+ */
+const mfrc522 = require("mfrc522-rpi");
+mfrc522.initWiringPi(0);
+
+console.log("scanning...");
+
+const run = setInterval(() => {
+
+    mfrc522.reset();
+
+    let response = mfrc522.findCard();
+
+    if (!response.status) {
+        return;
+    }
+
+    response = mfrc522.getUid();
+
+    if (!response.status) {
+        console.log("UID Scan Error");
+        return;
+    }
+
+    const uid = response.data;
+    console.log("Card read UID: %s %s %s %s", uid[0].toString(16), uid[1].toString(16), uid[2].toString(16), uid[3].toString(16));
+
+    mfrc522.stopCrypto();
+
+}, 500);
+
+
+
+module.exports = { run };
--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-const mfrc522 = require("mfrc522-rpi");
-//# Init WiringPi with SPI Channel 0
-mfrc522.initWiringPi(0);
+/**
+ * Local Modules
+ */
+const app = require('./application/bin/app');
 
-console.log("scanning...");
-
-setInterval(() => {
-
-    //# reset card
-    mfrc522.reset();
-
-    //# Scan for cards
-    let response = mfrc522.findCard();
-
-    if (!response.status) {
-        return;
-    }
-    //# Get the UID of the card
-    response = mfrc522.getUid();
-
-    if (!response.status) {
-        console.log("UID Scan Error");
-        return;
-    }
-
-    //# If we have the UID, continue
-    const uid = response.data;
-    console.log("Card read UID: %s %s %s %s", uid[0].toString(16), uid[1].toString(16), uid[2].toString(16), uid[3].toString(16));
-    
-    //# Stop
-    mfrc522.stopCrypto();
-
-}, 500);
+app.run();
